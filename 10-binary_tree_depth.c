308e3bf3065cf13b52a1962adddc23c177a83f7f
Tres Serio <treserio@hotmail.com>
2022-04-06 00:07:55 -0500

    10.1 rm excess var

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 /**
  * binary_tree_depth - find the height of the tree
- * @tree: root of the tree to measure
+ * @tree: node of the tree to measure
  * Return: a size_t of the number of levels below root
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0, parent;
+	size_t depth = 0;
 	/* exit case */
 	if (!tree)
 		return (0);
@@ -14,6 +14,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree->parent)
 		depth++;
 	/* measure depth of parent */
-	parent = binary_tree_depth(tree->parent);
-	return (depth + parent);
+	return (depth += binary_tree_depth(tree->parent));
 }
