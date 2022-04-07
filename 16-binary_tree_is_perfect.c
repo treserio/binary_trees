ccfbf6ed898138e74e9035ad70b2fe8d2df17bf9
Tres Serio <treserio@hotmail.com>
2022-04-06 21:49:24 -0500

    16.1 accounted for single node tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,7 +7,11 @@
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	/* exit case */
-	if (!tree || !tree->left || !tree->right)
+	if (!tree)
+		return (0);
+	if (!tree->parent && !tree->left && !tree->right)
+		return (1);
+	if (!tree->left || !tree->right)
 		return (0);
 	/* check that the node is a leaf */
 	if (!tree->left->left && !tree->left->right &&
@@ -15,5 +19,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	/* if a child exists return the and of it's children, if only one exists */
 	/* the exit case for it's other child will occur. */
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 }
